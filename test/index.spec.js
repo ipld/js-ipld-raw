52194d5c71b80cc07761f7e46e6ee7334320cc9a
David Dias <daviddias.p@gmail.com>
2017-10-06 13:45:27 +0300

    test: fix tests, use dirty-chai

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,19 +3,23 @@
 const IpfsBlock = require('ipfs-block')
 const CID = require('cids')
 const multihashing = require('multihashing-async')
-const expect = require('chai').expect
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
+chai.use(dirtyChai)
+
 const ipldRaw = require('../src/index')
 const resolver = ipldRaw.resolver
 
 describe('raw codec', () => {
-  let testData = new Buffer('test data')
+  let testData = Buffer.from('test data')
   let testIpfsBlock
 
   before((done) => {
     ipldRaw.util.serialize(testData, (err, result) => {
-      if (err) return done(err)
+      expect(err).to.not.exist()
       toIpfsBlock(resolver.multicodec, result, (err, ipfsBlock) => {
-        if (err) return done(err)
+        expect(err).to.not.exist()
         testIpfsBlock = ipfsBlock
         done()
       })
@@ -28,7 +32,7 @@ describe('raw codec', () => {
 
   it('resolver.resolve', () => {
     resolver.resolve(testIpfsBlock, 'a/b/c/d', (err, result) => {
-      expect(err).to.not.exist
+      expect(err).to.not.exist()
       expect(result.value.toString('hex')).to.equal(testData.toString('hex'))
       expect(result.remainderPath).to.equal('')
     })
@@ -36,15 +40,14 @@ describe('raw codec', () => {
 
   it('resolver.tree', () => {
     resolver.tree(testIpfsBlock, {}, (err, paths) => {
-      expect(err).to.not.exist
+      expect(err).to.not.exist()
       expect(Array.isArray(paths)).to.eql(true)
       expect(paths.length).to.eql(0)
     })
   })
-
 })
 
-function toIpfsBlock(multicodec, value, callback) {
+function toIpfsBlock (multicodec, value, callback) {
   multihashing(value, 'keccak-256', (err, hash) => {
     if (err) {
       return callback(err)
